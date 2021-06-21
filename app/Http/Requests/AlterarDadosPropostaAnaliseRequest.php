3f3469babdf9b61e3adf125a7c96bfe34d4ba518
Eliezer ECA. Cardoso Alves <eliezeralves@gac.local>
2021-06-21 14:07:33 -0300

    fix-api(rquest validator - alteracao dados proposta): corrigindo campos tipo empresa e cosif do validator de alterar dados da proposta pela tela de analise

--- app/Http/Requests/AlterarDadosPropostaAnaliseRequest.php
+++ app/Http/Requests/AlterarDadosPropostaAnaliseRequest.php
@@ -59,12 +59,13 @@ class AlterarDadosPropostaAnaliseRequest extends FormRequest
             'cliente.numero' => ['required', 'numeric'],
             'cliente.id_tipo_logradouro' => ['required', 'numeric', 'exists:cad_tipo_logradouro,id_tipo_logradouro'],
 
-            'cliente.tipo_empresa' => ['required', 'string'],
+            'cliente.id_tipo_empresa' => ['required', 'numeric', 'exists:cad_tipo_empresa,id_tipo_empresa'],
             'cliente.id_porte_empresa' => ['required', 'numeric', 'exists:cad_porte_empresa,id_porte_empresa'],
             'cliente.rendimento_mensal' => ['required', 'numeric', 'max:100000000'],
             'cliente.faturamento_anual' => ['required', 'numeric', 'max:100000000'],
             'cliente.capital_social' => ['required', 'numeric', 'max:100000000'],
             'cliente.ano_faturamento' => ['required', 'regex:/^[0-9]+$/u',  'size:4'],
+            'cliente.id_cosif' => ['required', 'regex:/^[0-9]+$/u',  'exists:cad_cosif,id_cosif'],
 
             'socios.0.id_pessoa_assinatura' => ['required', 'regex:/^[0-9]+$/u', 'exists:cad_pessoa_assinatura,id_pessoa_assinatura'],
             'socios.0.nome' => ['required', 'string'],
