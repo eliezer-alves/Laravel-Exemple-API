d30aaa70be4d8ae7b2ad0c579d580b7b7c6e69ac
Eliezer ECA. Cardoso Alves <eliezeralves@gac.local>
2021-06-28 14:42:10 -0300

    fix-api(cliente repository): corrigindo parametro cpf do metodo findByCpf da interface cliente reository

--- app/Repositories/Contracts/ClienteRepositoryInterface.php
+++ app/Repositories/Contracts/ClienteRepositoryInterface.php
@@ -7,7 +7,7 @@ interface ClienteRepositoryInterface extends AbstractRepositoryInterface
 
     public function findByCnpj(int $cnpj);
 
-    public function findByCpf(int $cnpj);
+    public function findByCpf(int $cpf);
 
     public function createWithUser(array $data);
 }
